fb69862390abd8e2c4cc9aca7f8373de6532ceae
Limen <Limen@limen.limen>
2021-11-11 20:24:27 +0800

    Add Book MVC and DB

---
+++ Data/BooksContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Books.Data
+{
+    public class BooksContext : DbContext
+    {
+        public BooksContext (DbContextOptions<BooksContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Book> Book { get; set; }
+    }
+}
